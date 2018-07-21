bcea8c0a2b89011ec037bdcc03f5ea0aafbb1dc7
prakash929523 <banapuramprakashreddy@gmail.com>
2018-07-21 19:05:39 -0400

    added ?completed filter

--- server.js
+++ server.js
@@ -13,10 +13,21 @@ app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
 
-
+//get /todos?completed=true
 app.get('/todos', function(req, res){
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}else if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	
+
 	//converting array to json format
-	res.json(todos);
+	res.json(filteredTodos);
 	//res.send(JSON.stringify(todos));
 });
 
@@ -69,6 +80,34 @@ app.delete('/todos/:id', function(req, res){
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+
+});
+
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId}); 
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) &&body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	 _.extend(matchedTodo, validAttributes);
+	 res.json(matchedTodo);
 
 });
 
