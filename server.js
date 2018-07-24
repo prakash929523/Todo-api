eaa3bbdf0dd262163905beeb84f813a01bb078f4
prakash929523 <banapuramprakashreddy@gmail.com>
2018-07-24 00:47:20 -0400

    convert GET /todo/:id to use sequelize

--- server.js
+++ server.js
@@ -42,21 +42,18 @@ app.get('/todos', function(req, res){
 //get todos by id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-		//(or)
-	// var matchedTodo;
 
-	// todos.forEach(function(todo){
-	// 	if(todoId === todo.id){
-	// 		matchedTodo =  todo;
-	// 	}
-	// });
 
-	if(matchedTodo){
-		res.json(matchedTodo);
-	} else{
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo){
+		if(!!todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
+
 });
 
 //POST /todos
@@ -70,18 +67,6 @@ app.post('/todos', function(req, res){
 		res.status(400).json(e);
 	});
 
-
-	// if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-	// 	return res.status(400).send();
-	// }
-
-	// body.description =body.description.trim();
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-
-	// res.json(body);
 });
 
 
