027205ee891818a04c565e2bbc04354c075329f1
prakash929523 <banapuramprakashreddy@gmail.com>
2018-07-24 17:50:04 -0400

    add login route and authunticate method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser =require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -120,9 +121,31 @@ app.put('/todos/:id', function(req, res){
 	 });
 });
 
+app.post('/users', function(req, res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user){
+		res.json(user.toPublicJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
+});
+
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user){
+		res.json(user.toPublicJSON());
+	}, function(){
+		res.status(401).send();
+	});
+
+	
+});
+
 
 //data base
-db.sequelize.sync().then(function(){
+db.sequelize.sync({force: true}).then(function(){
 	app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
 	});
