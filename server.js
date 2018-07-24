bb0007f1c8cc5e20b41908f9b386ebb055f245de
prakash929523 <banapuramprakashreddy@gmail.com>
2018-07-24 12:07:53 -0400

    convert delete

--- server.js
+++ server.js
@@ -73,15 +73,21 @@ app.post('/todos', function(req, res){
 //Delete http method /todos/:id
 app.delete('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId}); 
-
-	if(!matchedTodo){
-		res.status(404).json({"error" : "no todo found with that id"});
-	}else{
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
-
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted){
+		if(rowsDeleted === 0) {
+			res.status(404).json({
+				eror: 'No todo with id'
+			});
+		}else{
+				res.status(204).send();
+			}
+		}, function(){
+			res.status(500).send();
+ 	});
 });
 
 //PUT /todos/:id
